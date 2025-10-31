009f9ccec7a7cf23ca38eddb09886e33f0aeac20
dartanianfactory <romanwebdev93@gmail.com>
2025-10-31 14:25:18 +1000

    fixed front history

--- app/Http/Controllers/Web/WebPagesController.php
+++ app/Http/Controllers/Web/WebPagesController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers\Web;
 
 use App\Models\Common\GlobalSettings;
-use App\Models\User\Deposit;
 use App\Http\Controllers\Controller;
+use App\Models\User\DepositActions;
 use Illuminate\Http\Request;
 
 class WebPagesController extends Controller
 {
     public function home(Request $request)
     {
-        $depositHistory = Deposit::paginate(GlobalSettings::DEFAULT_PER_PAGES);
+        $depositHistory = DepositActions::orderBy('created_at', 'DESC')->paginate(GlobalSettings::DEFAULT_PER_PAGES);
 
         return view('pages.common.home', [
             'depositHistory' => $depositHistory,
--- resources/views/pages/common/home.blade.php
+++ resources/views/pages/common/home.blade.php
@@ -6,10 +6,13 @@
             <h3>Движения денежных средств</h3>
         </div>
         
-        <article>
+        <article class="mt-4">
             @forelse ($depositHistory as $rowHistory)
-                <div>
-                    <h3>{{ $rowHistory->user->email }}</h3>
+                <div class="mt-2">
+                    <h3>От: {{ $rowHistory->fromUser->email }}</h3>
+                    <h3>К: {{ $rowHistory->toUser->email }}</h3>
+                    <h3>Тип: {{ $rowHistory->type }}</h3>
+                    <h3>Сумма: {{ $rowHistory->amount }}</h3>
                 </div>
             @empty
                 <h3>Нет доступных записей</h3>
